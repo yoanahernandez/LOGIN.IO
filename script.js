3e48cef9df972a576d85128ff0ce1088c1bc1947
yoanahernandez <almahdzle@gmail.com>
2024-10-27 12:52:34 -0600

    subiendo archivos
    se anexan los archivos, index, script y style

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const errorMessage = document.getElementById('error-message');
+
+//Simulacion de credenciales de acceso
+const validUsername = 'yoana';
+const validPassword = '12345';
+
+form.addEventListener('submit' , function(e) {
+    e.preventDefault();
+
+    const enterdUsername = usernameInput.value.trim();
+    const enterdPassword = passwordInput.value;
+
+    if (enterdUsername === validUsername && enterdPassword === validPassword ) {
+        alert('Inicio de sesión exitoso!')
+        //Redirige a la pagina 
+        window.location.href = '';
+    }else{
+        alert('Credenciales incorrectas')
+    }
+})
