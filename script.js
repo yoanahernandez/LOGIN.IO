864bb5b09cde7bc576ccc3cd7700bc66f9cc6279
yoanahernandez <almahdzle@gmail.com>
2024-10-27 13:56:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ form.addEventListener('submit' , function(e) {
     if (enterdUsername === validUsername && enterdPassword === validPassword ) {
         alert('Inicio de sesión exitoso!')
         //Redirige a la pagina 
-        window.location.href = '';
+        window.location.href = '/CRUD/index.html';
     }else{
         alert('Credenciales incorrectas')
     }
